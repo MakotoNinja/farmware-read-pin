b11068bc13b39de947a57482f4103b9c28647dd2
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-20 17:27:44 -1000

    initial commit

--- script.py
+++ script.py
@@ -12,5 +12,5 @@ PIN_SENSOR = get_config_value(PKG, 'pin_num')
 get_pin = device.get_pin_value(PIN_SENSOR)
 device.log('Get Pin: {}'.format(get_pin))
 
-read_pin = device.read_pin(PIN_SENSOR, 'Sensor', 1)
+read_pin = device.read_pin(PIN_SENSOR, 'Moisture Sensor', 1)
 device.log('Read Pin: {}'.format(read_pin))
